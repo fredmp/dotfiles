3f7204bab27f98481c13299fac9963f240251433
Frederico Martins Porto <martinsporto@gmail.com>
2024-04-06 19:05:37 -0300

    Add vim test

---
+++ lua/plugins/vim-test.lua
@@ -0,0 +1,12 @@
+return {
+  'vim-test/vim-test',
+  dependencies = { 'preservim/vimux' },
+  config = function()
+    vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>', { desc = 'Test Nearest' })
+    vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = 'Test File' })
+    vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>', { desc = 'Test Suite' })
+    vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = 'Test Last' })
+    vim.keymap.set('n', '<leader>tb', ':TestVisit<CR>', { desc = 'Back to last test' })
+    vim.cmd "let test#strategy = 'vimux'"
+  end,
+}
