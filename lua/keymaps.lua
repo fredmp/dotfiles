5b544b9c4261de2d1d388bffa213c8232edf917f
Frederico Martins Porto <martinsporto@gmail.com>
2024-04-06 18:44:41 -0300

    Add tmux navigator

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,10 +7,10 @@ keymap.set({ 'n', 'i' }, '<C-Down>', ':bp<CR>')
 keymap.set({ 'n', 'i' }, '<C-Left>', ':bp<CR>')
 
 -- Navigate vim panes better
-keymap.set('n', '<c-k>', ':wincmd k<CR>')
-keymap.set('n', '<c-j>', ':wincmd j<CR>')
-keymap.set('n', '<c-h>', ':wincmd h<CR>')
-keymap.set('n', '<c-l>', ':wincmd l<CR>')
+--keymap.set('n', '<c-k>', ':wincmd k<CR>')
+--keymap.set('n', '<c-j>', ':wincmd j<CR>')
+--keymap.set('n', '<c-h>', ':wincmd h<CR>')
+--keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 -- Clean up search highlight
 keymap.set('n', '<leader>nh', ':nohlsearch<CR>')
